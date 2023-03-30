3b79f83614a45019bd5b27f8551e77631e868aba
Temptica <nicovsgaming@gmail.com>
2023-03-30 09:13:48 +0200

    Add device Scan (start and stop), Connecting, Disconnecting

---
+++ Bluetooth/BluetoothConnector.ts
@@ -0,0 +1,38 @@
+import react from "react";
+import {BleManager, Device } from 'react-native-ble-plx';
+
+export class BluetoothConnector{
+    static manager = new BleManager();
+    static foundBracelets = new  Array<Device>;
+
+    static scanDevices(){
+        this.manager.startDeviceScan(null, null,(error, device) =>{
+            if(error){
+                return;
+            }
+            if(device?.name?.includes('BIPSAB')){
+                this.foundBracelets.push(device);
+            }       
+        })
+    }
+    static stopDeviceScan(){
+        this.manager.stopDeviceScan();
+    }
+
+    static connectDevice(deviceId){
+        let device = this.foundBracelets.find((device)=> device.id === deviceId);
+        if(!device){
+            return "Nothing found";
+        }
+        try {
+            this.manager.connectToDevice(device.id);
+        } catch (error) {
+            return error
+        }
+    }
+    static disconnectDevice(deviceId){
+        this.manager.cancelDeviceConnection(deviceId).catch((error)=>{
+            return error
+        })
+    }
+}
